381934ee548381fcf16318bd989156ed402d958d
nickway90 <king.nickway90@gmail.com>
2018-05-02 18:08:14 +0800

    upload

--- src/util/sendEmail.js
+++ src/util/sendEmail.js
@@ -1,37 +1,22 @@
-const mandrill = require('mandrill-api/mandrill');
-const mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_KEY);
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'youremail@gmail.com',
+		pass: 'yourpassword',
+	},
+});
 
-module.exports = (subject, template_name, to, global_merge_vars) => {
-	var message = {
-		subject,
-		from_email: "noreply@domain.com",
-		from_name: "domain.com Team",
-		to: [{
-			email: to.email,
-			name: to.name || to.email.split("@")[0],
-			type: "to"
-		}],
-		global_merge_vars,
-		// [
-		// 	{
-		// 		name: 'VAR1',
-		// 		content: var1
-		// 	},
-		// 	{
-		// 		name: 'VAR2',
-		// 		content: var2,
-		// 	}
-		// ],
-	};
+module.exports = (to, subject, text) => {
+	const from = 'Your Name <youremail@gmail.com>';
+	const mailOptions = { from, to, subject, text };
 	return new Promise((resolve, reject) => {
-		mandrill_client.messages.sendTemplate({
-			template_name,
-			template_content: "",
-			message,
-		}, (result) => {
-			resolve(result)
-		}, (e) => {
-			reject(e)
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(info);
+			}
 		});
-	})
+	});
 };
